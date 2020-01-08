71f505452ad38505cc43b602638ede0715121aa3
Shakti Gurung <mr.shakti.gurung@gmail.com>
2020-01-08 17:13:27 +1100

    updated application to retrieve dynamic data from firestore and added HOC with-spinner

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -13,6 +13,8 @@ const firebaseConfig = {
     measurementId: "G-RFFVCVGTHK"
 };
 
+firebase.initializeApp(firebaseConfig);
+
 export const createUserProfileDocument = async (userAuth, additionalData) => {
     if(!userAuth) return;
 
@@ -35,11 +37,39 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
             console.log('error creating user', error.message);
         }
     }
-  
+    
     return userRef;
 }
 
-firebase.initializeApp(firebaseConfig);
+export const addCollectionAndDocuments =  async (collectionKey, objectsToAdd) => {
+    const collectionRef = firestore.collection(collectionKey);
+    
+    const batch = firestore.batch();
+    objectsToAdd.forEach(obj => {
+        const newDocRef = collectionRef.doc();
+        batch.set(newDocRef, obj);
+    });
+
+    return await batch.commit();
+};
+
+export const convertCollectionsSnapshotToMap = (collections) => {
+    const transformedCollection = collections.docs.map( doc => {
+        const {title, items} = doc.data();
+
+        return {
+            routeName: encodeURI(title.toLowerCase()),
+            id: doc.id,
+            title,
+            items
+        }
+    });
+    
+    return transformedCollection.reduce((accumulator, collection) => {
+        accumulator[collection.title.toLowerCase()] = collection;
+        return accumulator;
+    },{});
+}
 
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();

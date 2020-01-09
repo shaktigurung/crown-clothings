80f183ad056f69d1f2d8d0d38fc67ada85074001
Shakti Gurung <mr.shakti.gurung@gmail.com>
2020-01-09 12:16:21 +1100

    redux thunk added to the application and shop component refactored

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -10,10 +10,15 @@ export const selectCollections = createSelector(
 export const selectCollectionsForPreview = createSelector(
     [selectCollections],
     collections => collections ? Object.keys(collections).map(key=> collections[key]) : []
-)
+);
 
 export const selectCollection = collectionUrlParam => 
     createSelector(
         [selectCollections],
         collections => (collections ? collections[collectionUrlParam] : null)
-)
+);
+
+export const selectIsCollectionFetching = createSelector(
+    [selectShop],
+    shop => shop.isFetching
+);

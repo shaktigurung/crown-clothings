03770bcff200e54ef0521cf4a5c58cc868836494
Shakti Gurung <mr.shakti.gurung@gmail.com>
2020-01-18 18:38:06 +1100

    sign in with email and google fixed with saga

--- src/App.js
+++ src/App.js
@@ -9,8 +9,6 @@ import Header from './components/header/header.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import CheckoutPage from './pages/checkout/checkout.component';
 
-import {auth, createUserProfileDocument} from './firebase/firebase.utils';
-import {setCurrentUser} from './redux/user/user.actions';
 import {selectCurrentUser} from './redux/user/user.selectors';
 
 import './App.css';
@@ -19,21 +17,22 @@ class App extends React.Component {
   unsubscribeFromAuth = null
 
   componentDidMount() {
-    const {setCurrentUser} = this.props;
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
-      if(userAuth) {
-        const userRef = await createUserProfileDocument(userAuth);
+   
 
-        userRef.onSnapshot(snapShot => {
-          setCurrentUser({
-            id: snapShot.id,
-            ...snapShot.data()
-          });
-        });
-      } else {
-        setCurrentUser(userAuth);
-      }
-    }); 
+    // this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+    //   if(userAuth) {
+    //     const userRef = await createUserProfileDocument(userAuth);
+
+    //     userRef.onSnapshot(snapShot => {
+    //       setCurrentUser({
+    //         id: snapShot.id,
+    //         ...snapShot.data()
+    //       });
+    //     });
+    //   } else {
+    //     setCurrentUser(userAuth);
+    //   }
+    // }); 
   }
 
   componentWillUnmount() {
@@ -68,8 +67,5 @@ const mapStateToProps = createStructuredSelector({
   currentUser: selectCurrentUser
 });
 
-const mapDispatchToProps = dispatch => ({
-  setCurrentUser: user => dispatch(setCurrentUser(user))
-});
 
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default connect(mapStateToProps)(App);
--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import FormInput from './../form-input/form-input.component';
 import CustomButton from './../custom-button/custom-button.component';
-import {auth, signInWithGoogle} from './../../firebase/firebase.utils';
+import { googleSignInStart, emailSignInStart } from './../../redux/user/user.actions';
 
 import {SignInContainer, SignInTitle, ButtonsBarContainer} from './sign-in.styles';
 
@@ -16,24 +17,20 @@ class SignIn extends React.Component {
 
     handleSubmit = async event => {
         event.preventDefault();
+        const { emailSignInStart } = this.props;
         const {email, password}= this.state;
 
-        try {
-            await auth.signInWithEmailAndPassword(email,password);
-            this.setState({ email: '', password: ''})
-        } catch(error) {
-            console.log(error);
-        }
-
-        this.setState({email:'', password:''});
-    }
+        emailSignInStart(email, password);
+    };
 
     handleChange = event => {
         const { name,value } = event.target;
 
         this.setState({ [name]: value });
-    }
+    };
+    
     render(){
+        const { googleSignInStart } = this.props;
         return(
             <SignInContainer>
                 <SignInTitle>I already have an account</SignInTitle>
@@ -58,7 +55,7 @@ class SignIn extends React.Component {
                     />
                     <ButtonsBarContainer>
                         <CustomButton type='submit'> Sign in </CustomButton>
-                        <CustomButton onClick={signInWithGoogle} isGoogleSignIn> Sign in with Google </CustomButton>
+                        <CustomButton type='button' onClick={googleSignInStart} isGoogleSignIn> Sign in with Google </CustomButton>
                     </ButtonsBarContainer>
                 </form>
             </SignInContainer>
@@ -66,4 +63,8 @@ class SignIn extends React.Component {
     }
 }
 
-export default SignIn;
+const mapDispatchToPros = dispatch => ({
+    googleSignInStart: () => dispatch(googleSignInStart()),
+    emailSignInStart: (email, password) => dispatch(emailSignInStart({ email, password} ))
+})
+export default connect(null, mapDispatchToPros)(SignIn);
